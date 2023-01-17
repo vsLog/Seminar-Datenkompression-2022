a499752a3da7a2f8b1fcfd40ee8c441ab7db165d
Mario Navarro Krauß <s0884273@rz.uni-frankfurt.de>
2023-01-17 15:54:53 +0100

    Added MIT License

--- HTML/scripts/decoding.js
+++ HTML/scripts/decoding.js
@@ -1,3 +1,35 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Mark Thomas Nelson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+ This code was written by Mario Navarro Krauß and Vito Schopper to illustrate
+ and visualize the arithmetic coding algorithm used for Data Compression.
+ The encoding and decoding scripts use parts of the algorithm descriped 
+ by Mark Nelson' article:
+ Data Compression With Arithmetic Coding
+ published at: http://marknelson.us/2014/10/19/data-compression-with-arithmetic-coding
+
+*/
+
 decodeInput = function() {
     var input = Number(document.getElementById("output_area").value);
     totalChars = sessionStorage.getItem("totalChars");
